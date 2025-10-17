65b5f4d1f40393f24404463e8086b0e35027947a
Alan Somers <asomers@gmail.com>
2025-10-17 10:25:43 -0600

    Convert the exporter to use axum
    This replaces prometheus-exporter/tiny-http with prometheus/axum. The
    former pair are unmaintained and have bugs with no released solutions.
    
    Also bring in the anyerror crate, which helps to bridge
    freebsd-libgeom's error types to those that axum needs.
    
    Fixes #54

--- freebsd-geom-exporter/src/main.rs
+++ freebsd-geom-exporter/src/main.rs
@@ -1,15 +1,26 @@
 // vim: tw=80
 use std::{
-    error::Error,
     net::{IpAddr, SocketAddr},
     process::exit,
+    sync::{Arc, LazyLock},
 };
 
-use clap::Parser;
+use axum::{
+    extract::State,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
+use clap::{
+    error::{Error as ClapError, ErrorKind as ClapErrorKind},
+    Parser,
+};
 use env_logger::{Builder, Env};
 use freebsd_libgeom::{Snapshot, Statistics, Tree};
-use prometheus_exporter::prometheus::register_gauge_vec;
+use prometheus::{register_gauge_vec, GaugeVec, TextEncoder};
 use regex::Regex;
+use tokio::net::TcpListener;
 
 /// Export GEOM device metrics to Prometheus
 #[derive(Debug, Default, clap::Parser)]
@@ -21,17 +32,160 @@ struct Cli {
     #[clap(short = 'P', long = "physical")]
     physical: bool,
     /// Only report devices with names matching this regex.
-    #[clap(short = 'f', long = "include")]
-    include:  Option<String>,
+    #[clap(short = 'f', long = "include", value_parser = regex_parser)]
+    include:  Option<Regex>,
     /// Do not report devices with names matching this regex
-    #[clap(short = 'F', long = "exclude")]
-    exclude:  Option<String>,
+    #[clap(short = 'F', long = "exclude", value_parser = regex_parser)]
+    exclude:  Option<Regex>,
     /// TCP port
     #[clap(short = 'p', default_value = "9248")]
     port:     u16,
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn regex_parser(s: &str) -> Result<Regex, ClapError> {
+    match Regex::new(s) {
+        Ok(s) => Ok(s),
+        Err(e) => Err(ClapError::raw(ClapErrorKind::ValueValidation, e)),
+    }
+}
+
+static BUSY_TIME: LazyLock<GaugeVec> = LazyLock::new(|| {
+    register_gauge_vec!(
+        "geom_busy_time",
+        "Cumulative time in seconds that the device had at least one \
+         outstanding operation",
+        &["device"],
+    )
+    .expect("cannot create gauge")
+});
+static BYTES: LazyLock<GaugeVec> = LazyLock::new(|| {
+    register_gauge_vec!(
+        "geom_bytes",
+        "Total bytes processed",
+        &["device", "method"]
+    )
+    .expect("cannot create gauge")
+});
+static DURATION: LazyLock<GaugeVec> = LazyLock::new(|| {
+    register_gauge_vec!(
+        "geom_duration",
+        "Total time spent processing commands in seconds",
+        &["device", "method"]
+    )
+    .expect("cannot create gauge")
+});
+static OPS: LazyLock<GaugeVec> = LazyLock::new(|| {
+    register_gauge_vec!(
+        "geom_operations",
+        "Total operations processed",
+        &["device", "method"]
+    )
+    .expect("cannot create gauge")
+});
+static QUEUE_LENGTH: LazyLock<GaugeVec> = LazyLock::new(|| {
+    register_gauge_vec!(
+        "geom_queue_length",
+        "Number of incomplete transactions at the sampling instant",
+        &["device"]
+    )
+    .expect("cannot create gauge")
+});
+
+/// Wrapper type that implements IntoResponse for anyhow::Error.
+#[derive(Debug)]
+struct AppError(anyhow::Error);
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (StatusCode::INTERNAL_SERVER_ERROR, format!("{}", self.0))
+            .into_response()
+    }
+}
+
+async fn metrics(cli: State<Arc<Cli>>) -> Result<String, AppError> {
+    // inner relies on an implicit Into conversion to return anyhow::Error
+    let inner = || -> Result<String, anyhow::Error> {
+        // Note: it might be more efficient to only call Tree:new if we detect
+        // that a device has arrived or departed.  But on a system with hundreds
+        // of disks, it only takes 13ms.
+        let mut tree = Tree::new()?;
+        let mut current = Snapshot::new()?;
+        BUSY_TIME.reset();
+
+        for item in current.iter() {
+            if let Some(gident) = tree.lookup(item.id()) {
+                if let Some(rank) = gident.rank() {
+                    if rank > 1 && cli.physical {
+                        continue;
+                    }
+                    let device = gident.name().unwrap().to_string_lossy();
+                    if !cli
+                        .include
+                        .as_ref()
+                        .map(|f| f.is_match(&device))
+                        .unwrap_or(true)
+                    {
+                        continue;
+                    }
+                    if cli
+                        .exclude
+                        .as_ref()
+                        .map(|f| f.is_match(&device))
+                        .unwrap_or(false)
+                    {
+                        continue;
+                    }
+                    let stats = Statistics::compute(item, None, 0.0);
+
+                    BUSY_TIME
+                        .with_label_values(&[&device])
+                        .set(stats.busy_time());
+                    QUEUE_LENGTH
+                        .with_label_values(&[&device])
+                        .set(stats.queue_length() as f64);
+                    BYTES
+                        .with_label_values(&[&*device, "read"])
+                        .set(stats.total_bytes_read() as f64);
+                    DURATION
+                        .with_label_values(&[&*device, "read"])
+                        .set(stats.total_duration_read());
+                    OPS.with_label_values(&[&*device, "read"])
+                        .set(stats.total_transfers_read() as f64);
+                    BYTES
+                        .with_label_values(&[&*device, "write"])
+                        .set(stats.total_bytes_write() as f64);
+                    DURATION
+                        .with_label_values(&[&*device, "write"])
+                        .set(stats.total_duration_write());
+                    OPS.with_label_values(&[&*device, "write"])
+                        .set(stats.total_transfers_write() as f64);
+                    BYTES
+                        .with_label_values(&[&*device, "free"])
+                        .set(stats.total_bytes_free() as f64);
+                    DURATION
+                        .with_label_values(&[&*device, "free"])
+                        .set(stats.total_duration_free());
+                    OPS.with_label_values(&[&*device, "free"])
+                        .set(stats.total_transfers_free() as f64);
+                    DURATION
+                        .with_label_values(&[&*device, "other"])
+                        .set(stats.total_duration_other());
+                    OPS.with_label_values(&[&*device, "other"])
+                        .set(stats.total_transfers_other() as f64);
+                }
+            }
+        }
+        let metric_families = prometheus::gather();
+        let encoder = TextEncoder::new();
+        let body = encoder.encode_to_string(&metric_families)?;
+        Ok(body)
+    };
+    // Now convert the error type again.
+    inner().map_err(AppError)
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
     let cli: Cli = Cli::parse();
 
     // Setup logger with default level info so we can see the messages from
@@ -45,128 +199,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     });
     let sa = SocketAddr::new(ia, cli.port);
 
-    let include = cli.include.as_ref().map(|s| {
-        Regex::new(s).unwrap_or_else(|e| {
-            eprintln!("Cannot parse include regex: {e}");
-            exit(2);
-        })
-    });
-    let exclude = cli.exclude.as_ref().map(|s| {
-        Regex::new(s).unwrap_or_else(|e| {
-            eprintln!("Cannot parse exclude regex: {e}");
-            exit(2);
-        })
-    });
+    let app = Router::new()
+        .route("/metrics", get(metrics))
+        // Annoyingly, with_state requires its argument to be `Send` even if
+        // we're using a single-threaded runtime.  So we must use Arc instead of
+        // Rc.
+        .with_state(Arc::new(cli));
 
-    let exporter = prometheus_exporter::start(sa).unwrap_or_else(|e| {
-        eprintln!("Error starting exporter: {e}");
-        exit(1);
-    });
-
-    let duration = register_gauge_vec!(
-        "geom_duration",
-        "Total time spent processing commands in seconds",
-        &["device", "method"]
-    )
-    .expect("cannot create gauge");
-    let bytes = register_gauge_vec!(
-        "geom_bytes",
-        "Total bytes processed",
-        &["device", "method"]
-    )
-    .expect("cannot create gauge");
-    let ops = register_gauge_vec!(
-        "geom_operations",
-        "Total operations processed",
-        &["device", "method"]
-    )
-    .expect("cannot create gauge");
-    let busy_time = register_gauge_vec!(
-        "geom_busy_time",
-        "Cumulative time in seconds that the device had at least one \
-         outstanding operation",
-        &["device"]
-    )
-    .expect("cannot create gauge");
-    let queue_length = register_gauge_vec!(
-        "geom_queue_length",
-        "Number of incomplete transactions at the sampling instant",
-        &["device"]
-    )
-    .expect("cannot create gauge");
-
-    loop {
-        let _guard = exporter.wait_request();
-        // Note: it might be more efficient to only call Tree:new if we detect
-        // that a device has arrived or departed.  But on a system with hundreds
-        // of disks, it only takes 13ms.
-        let mut tree = Tree::new()?;
-        let mut current = Snapshot::new()?;
-        busy_time.reset();
-        duration.reset();
-        bytes.reset();
-        ops.reset();
-        queue_length.reset();
-        for item in current.iter() {
-            if let Some(gident) = tree.lookup(item.id()) {
-                if let Some(rank) = gident.rank() {
-                    if rank > 1 && cli.physical {
-                        continue;
-                    }
-                    let device = gident.name().unwrap().to_string_lossy();
-                    if !include
-                        .as_ref()
-                        .map(|f| f.is_match(&device))
-                        .unwrap_or(true)
-                    {
-                        continue;
-                    }
-                    if exclude
-                        .as_ref()
-                        .map(|f| f.is_match(&device))
-                        .unwrap_or(false)
-                    {
-                        continue;
-                    }
-                    let stats = Statistics::compute(item, None, 0.0);
-
-                    busy_time
-                        .with_label_values(&[&device])
-                        .set(stats.busy_time());
-                    queue_length
-                        .with_label_values(&[&device])
-                        .set(stats.queue_length() as f64);
-                    bytes
-                        .with_label_values(&[&device, "read"])
-                        .set(stats.total_bytes_read() as f64);
-                    duration
-                        .with_label_values(&[&device, "read"])
-                        .set(stats.total_duration_read());
-                    ops.with_label_values(&[&device, "read"])
-                        .set(stats.total_transfers_read() as f64);
-                    bytes
-                        .with_label_values(&[&device, "write"])
-                        .set(stats.total_bytes_write() as f64);
-                    duration
-                        .with_label_values(&[&device, "write"])
-                        .set(stats.total_duration_write());
-                    ops.with_label_values(&[&device, "write"])
-                        .set(stats.total_transfers_write() as f64);
-                    bytes
-                        .with_label_values(&[&device, "free"])
-                        .set(stats.total_bytes_free() as f64);
-                    duration
-                        .with_label_values(&[&device, "free"])
-                        .set(stats.total_duration_free());
-                    ops.with_label_values(&[&device, "free"])
-                        .set(stats.total_transfers_free() as f64);
-                    duration
-                        .with_label_values(&[&device, "other"])
-                        .set(stats.total_duration_other());
-                    ops.with_label_values(&[&device, "other"])
-                        .set(stats.total_transfers_other() as f64);
-                }
-            }
-        }
-    }
+    let listener = TcpListener::bind(sa).await.unwrap();
+    axum::serve(listener, app).await.unwrap()
 }
